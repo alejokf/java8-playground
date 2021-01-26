1adf31b79f3f4c211ec960745be5e84e6631f77c
Alejandro Marulanda <alejandro.marulanda@netcentric.biz>
2021-01-26 10:46:09 +0100

    Add default method to interface

--- src/main/java/interfaces/Figure3D.java
+++ src/main/java/interfaces/Figure3D.java
@@ -2,5 +2,11 @@ package interfaces;
 
 public interface Figure3D {
 
+    double GOLD_DENSITY = 19.3;
+
     double volume();
+
+    default double weight() {
+        return volume() * GOLD_DENSITY;
+    }
 }
--- src/main/java/interfaces/FigureMain.java
+++ src/main/java/interfaces/FigureMain.java
@@ -13,6 +13,7 @@ public class FigureMain {
     private static void logFigure(Figure3D figure3D) {
         System.out.println(String.format("This %s has yhe following values:", figure3D.getClass().getSimpleName()));
         System.out.println(String.format("Volume: %s", figure3D.volume()));
+        System.out.println(String.format("Weight: %s", figure3D.weight()));
         System.out.println();
     }
 }
